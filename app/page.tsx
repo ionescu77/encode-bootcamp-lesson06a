b936b4d127b4ed5172a5614fe96d4541c33275a4
Razvan Ionescu <ionescu77@gmail.com>
2024-08-28 13:52:33 +0300

    Lesson6 Exercise1 P4-P5-P6-P7 add style to messages, show if waiting for AI backend processing

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,35 @@
 import { useChat } from "ai/react";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    isLoading,
+    append,
+  } = useChat();
   return (
     <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch overflow-hidden">
       <div className="overflow-auto w-full">
         {messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap">
+          <div
+            key={m.id}
+            className={`whitespace-pre-wrap ${
+              m.role === "user"
+                ? "bg-green-700 p-3 m-2 rounded-lg"
+                : "bg-slate-700 p-3 m-2 rounded-lg"
+            }`}
+          >
             {m.role === "user" ? "User: " : "AI: "}
             {m.content}
           </div>
         ))}
+        {isLoading && (
+          <div className="flex justify-end pr-4">
+            <span className="animate-pulse text-2xl">...</span>
+          </div>
+        )}
       </div>
 
       <form onSubmit={handleSubmit}>
